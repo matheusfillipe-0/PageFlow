b937fd378c2cc79a1b99575c97daebf31f298738
Mateus Filipe <135716569+matheusfillipe-0@users.noreply.github.com>
2024-10-17 13:45:02 -0300

    Update nav.js

--- nav/nav.js
+++ nav/nav.js
@@ -24,11 +24,38 @@ function toggleTheme() {
     footer.classList.remove('dark-mode');
     footer.classList.add('light-mode');
     themeButton.textContent = '☀️';  
+    localStorage.setItem('theme', 'light');  // Armazena tema claro
   } else {
     body.classList.remove('light-mode');
     body.classList.add('dark-mode');
     footer.classList.remove('light-mode');
     footer.classList.add('dark-mode');
     themeButton.textContent = '🌙'; 
+    localStorage.setItem('theme', 'dark');  // Armazena tema escuro
   }
+}
+
+// Aplica o tema salvo ao carregar a página
+window.onload = function() {
+  const savedTheme = localStorage.getItem('theme');
+  const body = document.body;
+  const themeButton = document.querySelector('.control-theme');
+  const footer = document.querySelector('.footer');
+
+  if (savedTheme === 'dark') {
+    body.classList.add('dark-mode');
+    footer.classList.add('dark-mode');
+    themeButton.textContent = '🌙'; 
+  } else {
+    body.classList.add('light-mode');
+    footer.classList.add('light-mode');
+    themeButton.textContent = '☀️';
+  }
+};
+
+
+
+function reloadPage() {
+  console.log('Recarregar página chamado');
+  window.location.reload();  
 }
